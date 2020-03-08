9dc228875466bfc1343f4231216eb668ea48c1c4
Serigne DIENG <smbdieng@gmail.com>
2020-03-08 13:16:55 +0100

    first commit

---
+++ src/main/java/com/khidmasoft/sbdocjib/SbDocJibApplication.java
@@ -0,0 +1,13 @@
+package com.khidmasoft.sbdocjib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDocJibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDocJibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khidmasoft/sbdocjib/SbDocJibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khidmasoft.sbdocjib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDocJibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
